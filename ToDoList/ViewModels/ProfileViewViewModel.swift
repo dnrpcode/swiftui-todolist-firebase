b1a4d3bf54561e6b1058f59f91e939b4ca774e90
DNRP Code <dnrpcode@gmail.com>
2023-08-28 06:31:15 +0700

    add ui profile and integration logOut

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -12,22 +12,35 @@ import Foundation
 
 class ProfileViewViewModel: ObservableObject {
     init() {}
+    @Published var user: User? = nil
     
-    func toggleIsDone(item: ToDoListItem){
-        var itemCopy = item
-        itemCopy.setDone(item.isDone)
-        
-        guard let uid = Auth.auth().currentUser?.uid else {
+    func fetchUser() {
+        guard let userId = Auth.auth().currentUser?.uid else {
             return
         }
-        
         let db = Firestore.firestore()
-        db.collection("users")
-            .document(uid)
-            .collection("todos")
-            .document(itemCopy.id)
-            .setData(itemCopy.asDictionary())
-        
+        db.collection("users").document(userId).getDocument{ [weak self] snapshot, error in
+            guard let data = snapshot?.data(), error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self?.user = User(
+                    id: data["id"] as? String ?? "",
+                    name: data["name"] as? String ?? "",
+                    email: data["email"] as? String ?? "",
+                    joined: data["joined"] as? TimeInterval ?? 0
+                )
+            }
+        }
+    }
+    
+    func logOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error)
+        }
     }
     
 }
--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -13,9 +13,54 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
-                
-            }.navigationTitle("Profile")
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading...")
+                }
+            }
+            .navigationTitle("Profile")
         }
+        .onAppear{
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        VStack(alignment: .leading){
+            HStack{
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            HStack{
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            HStack{
+                Text("Member Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+        }
+        .padding()
+        
+        Button("LogOut"){
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        
+        Spacer()
     }
 }
 
