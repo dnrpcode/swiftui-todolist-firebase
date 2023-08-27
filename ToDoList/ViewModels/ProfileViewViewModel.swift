a09766c1248c6e78dbdcf1cd35078048682745af
DNRP Code <dnrpcode@gmail.com>
2023-08-28 06:01:44 +0700

    integration update status todo list

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by dani prayogi on 26/08/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 
@@ -12,6 +14,19 @@ class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem){
+        var itemCopy = item
+        itemCopy.setDone(item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
         
     }
     
